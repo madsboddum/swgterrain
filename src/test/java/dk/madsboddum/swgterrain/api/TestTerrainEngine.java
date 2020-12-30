73af3be9a94c7ac5789bbcdb743feee4e97c58c5
Ziggy <ziggyswg@outlook.com>
2020-12-30 15:12:59 +0100

    Fix issue where Dantooine was unloadable #1

--- src/test/java/dk/madsboddum/swgterrain/api/TestTerrainEngine.java
+++ src/test/java/dk/madsboddum/swgterrain/api/TestTerrainEngine.java
@@ -1,47 +1,80 @@
 package dk.madsboddum.swgterrain.api;
 
-import org.junit.Before;
-import org.junit.Test;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.io.File;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.URL;
 
-import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class TestTerrainEngine {
 	
 	private TerrainEngine engine;
 	
-	@Before
-	public void setup() throws URISyntaxException {
-		URL resource = TestTerrainEngine.class.getClassLoader().getResource("corellia.trn");	// Corellia has both land and water. Useful.
+	private static TerrainEngine createEngine(String fileName) throws URISyntaxException {
+		URL resource = TestTerrainEngine.class.getClassLoader().getResource(fileName);
 		assert resource != null;
 		URI uri = resource.toURI();
 		
-		engine = new TerrainEngine(new File(uri));
+		return new TerrainEngine(new File(uri));
 	}
 	
-	@Test
-	public void testCanReadHeightCoordinate() {
-		double actual = engine.getHeight(1234, 3214);
-		double expected = 141.53;
-		
-		assertEquals("Unexpected height coordinate", expected, actual, 0.01);
+	@BeforeEach
+	public void setup() throws URISyntaxException {
+		engine = createEngine("corellia.trn");
 	}
 	
-	@Test
-	public void testCanDetectWaterPositive() {
-		boolean water = engine.isWater(6700, 2700);
+	@Nested
+	class HeightCoordinateReading {
 		
-		assertTrue("The ocean on Corellia should be considered water", water);
+		@Test
+		public void testCanReadHeightCoordinate() {
+			double actual = engine.getHeight(1234, 3214);
+			double expected = 141.53;
+			
+			assertEquals(expected, actual, 0.01, "Unexpected height coordinate");
+		}
 	}
 	
-	@Test
-	public void testCanDetectWaterNegative() {
-		boolean water = engine.isWater(-6600, 2100);
+	@Nested
+	class WaterDetection {
 		
-		assertFalse("The mountains on Corellia should not be considered water", water);
+		@Test
+		public void testCanDetectWaterPositive() {
+			boolean water = engine.isWater(6700, 2700);
+			
+			assertTrue(water, "The ocean on Corellia should be considered water");
+		}
+		
+		@Test
+		public void testCanDetectWaterNegative() {
+			boolean water = engine.isWater(-6600, 2100);
+			
+			assertFalse(water, "The mountains on Corellia should not be considered water");
+		}
+	
+	}
+	
+	@Nested
+	class SupportsTerrains {
+		
+		@ParameterizedTest(name = "{0}")
+		@ValueSource(strings = {
+				"corellia.trn",
+				"dantooine.trn",	// Dantooine has some odd empty nodes that require special handling
+		})
+		public void load(String fileName) throws URISyntaxException {
+			TerrainEngine engine = createEngine(fileName);
+			
+			// Ensure that the terrain's actually being loaded, even if this logic is later moved out of the constructor of TerrainEngine
+			engine.getHeight(0, 0);
+		}
 	}
 }
