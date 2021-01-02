9b0c9f1f4fd040776468d82402dbf81e436d40da
Ziggy <ziggyswg@outlook.com>
2021-01-02 06:14:22 +0100

    Code formatting for AffectorHeightConstant

--- src/main/java/dk/madsboddum/swgterrain/internal/terrainDetail/layers/AffectorHeightConstant.java
+++ src/main/java/dk/madsboddum/swgterrain/internal/terrainDetail/layers/AffectorHeightConstant.java
@@ -5,8 +5,7 @@ import org.apache.mina.core.buffer.IoBuffer;
 
 public class AffectorHeightConstant extends HeightLayer {
 
-	int   transform_type;
-	//float height_val;
+	int transform_type;
 	
 	@Override
 	public float process(float x, float z, float transform_value, float base_height, TerrainVisitor ti) {
@@ -15,22 +14,21 @@ public class AffectorHeightConstant extends HeightLayer {
 			return base_height;
 		
 		float result;
-		switch (transform_type) 
-		{
-		case 1:
-			result = transform_value * height_val + base_height;
-			break;
-		case 2:
-			result = base_height - transform_value * height_val;
-			break;
-		case 3:
-			result = base_height + (base_height * height_val - base_height) * transform_value;
-			break;
-		case 4:
-			result = 0;
-			break;
-		default:
-			result = (float) ((1.0 - transform_value) * base_height + transform_value * height_val);
+		switch (transform_type) {
+			case 1:
+				result = transform_value * height_val + base_height;
+				break;
+			case 2:
+				result = base_height - transform_value * height_val;
+				break;
+			case 3:
+				result = base_height + (base_height * height_val - base_height) * transform_value;
+				break;
+			case 4:
+				result = 0;
+				break;
+			default:
+				result = (float) ((1.0 - transform_value) * base_height + transform_value * height_val);
 		}
 	
 		return result;
